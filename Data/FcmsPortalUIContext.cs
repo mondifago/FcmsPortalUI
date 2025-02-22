2a6e143605da4d90e9f6c993cb83d040c78abb1b
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-22 23:05:25 +1100

    Added Address component, address service manager with external data test

---
+++ Data/FcmsPortalUIContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FcmsPortalUI.Data
+{
+    public class FcmsPortalUIContext : DbContext
+    {
+        public FcmsPortalUIContext(DbContextOptions<FcmsPortalUIContext> options)
+            : base(options)
+        { }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using FcmsPortal.Services;
 using FcmsPortalUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<AddressService>();
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +19,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
