38655acc8fcc8449fa17f9e35d0af734e0393805
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-03-12 04:12:59 +1100

    removed static methods from Guardian Service, modified sample data in staff and transfer to staff service

--- Services/StaffService.cs
+++ Services/StaffService.cs
@@ -1,21 +1,75 @@
-﻿using FcmsPortal.Models;
+﻿using FcmsPortal.Enums;
+using FcmsPortal.Models;
 
 namespace FcmsPortalUI.Services
 {
     public class StaffService
     {
-        private List<Staff> _staffList = new();
+        private readonly List<Staff> _staffList = new();
+
+        public StaffService()
+        {
+            // Create admin staff
+            Staff staff1 = new Staff
+            {
+                Id = 1,
+                Person = new Person
+                {
+                    FirstName = "Mr. Fin",
+                    MiddleName = "F",
+                    LastName = "Fen",
+                    Email = "fin@fcms.com",
+                    PhoneNumber = "08012345678",
+                },
+                JobRole = JobRole.Admin,
+                JobDescription = "Principal"
+            };
+
+            // Create Biology teacher
+            Staff staff2 = new Staff
+            {
+                Id = 2,
+                Person = new Person
+                {
+                    FirstName = "Mr Eric",
+                    MiddleName = "E",
+                    LastName = "Een",
+                    EducationLevel = EducationLevel.SeniorCollege,
+                    ClassLevel = ClassLevel.SC_3
+                },
+                JobRole = JobRole.Teacher,
+                JobDescription = "Biology Teacher",
+                AreaOfSpecialization = CourseDefaults.GetCourseNames(EducationLevel.SeniorCollege)[3]
+            };
+
+            // Create Geography teacher
+            Staff staff3 = new Staff
+            {
+                Id = 3,
+                Person = new Person
+                {
+                    FirstName = "Mrs Qin",
+                    MiddleName = "Q",
+                    LastName = "Que",
+                    EducationLevel = EducationLevel.SeniorCollege,
+                    ClassLevel = ClassLevel.SC_3
+                },
+                JobRole = JobRole.Teacher,
+                JobDescription = "Geography Teacher",
+                AreaOfSpecialization = CourseDefaults.GetCourseNames(EducationLevel.SeniorCollege)[12]
+            };
+
+            _staffList.Add(staff1);
+            _staffList.Add(staff2);
+            _staffList.Add(staff3);
+        }
+
         private int _nextId = 1;
 
-        public async Task<List<Staff>> GetGuardiansAsync()
+        public async Task<List<Staff>> GetAllStaffAsync()
         {
             await Task.Delay(100);
             return _staffList;
-        }
-
-        public Task<List<Staff>> GetAllStaffAsync()
-        {
-            return Task.FromResult(_staffList);
         }
 
         public Task<Staff?> GetStaffByIdAsync(int id)
