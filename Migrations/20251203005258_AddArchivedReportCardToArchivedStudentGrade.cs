417aa13661f052f76f87055ed1af9fe66fbcc12e
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-12-03 11:55:47 +1100

    fix #395 Add Report Card Property to ArchivedStudentGrade

---
+++ Migrations/20251203005258_AddArchivedReportCardToArchivedStudentGrade.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FcmsPortalUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArchivedReportCardToArchivedStudentGrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ArchivedReportCardId",
+                table: "ArchivedStudentGrades",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArchivedStudentGrades_ArchivedReportCardId",
+                table: "ArchivedStudentGrades",
+                column: "ArchivedReportCardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ArchivedStudentGrades_StudentReportCards_ArchivedReportCardId",
+                table: "ArchivedStudentGrades",
+                column: "ArchivedReportCardId",
+                principalTable: "StudentReportCards",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ArchivedStudentGrades_StudentReportCards_ArchivedReportCardId",
+                table: "ArchivedStudentGrades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ArchivedStudentGrades_ArchivedReportCardId",
+                table: "ArchivedStudentGrades");
+
+            migrationBuilder.DropColumn(
+                name: "ArchivedReportCardId",
+                table: "ArchivedStudentGrades");
+        }
+    }
+}
