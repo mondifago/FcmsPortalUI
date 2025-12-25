b315f62c308d0e82a29087ee67a5062a3d93f267
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-12-26 00:51:17 +1100

    fix #427 Personalize Class Session reports to show the name of teacher that submitted the report

---
+++ Migrations/20251225133210_AddRemarksSubmissionTracking.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FcmsPortalUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemarksSubmissionTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RemarksSubmittedAt",
+                table: "ClassSessions",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RemarksSubmittedByName",
+                table: "ClassSessions",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemarksSubmittedAt",
+                table: "ClassSessions");
+
+            migrationBuilder.DropColumn(
+                name: "RemarksSubmittedByName",
+                table: "ClassSessions");
+        }
+    }
+}
