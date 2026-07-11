30692cf80654a7aff37b9801a69206f94914c68f
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-07-11 18:51:30 +1000

    fix #478 Re-design Report Page and fix bugs on it

--- DTOs/GradesReport.cs
+++ DTOs/GradesReport.cs
@@ -5,15 +5,9 @@ namespace FcmsPortalUI.DTOs
     public class GradesReport
     {
         public int LearningPathId { get; set; }
-
         public string LearningPathName { get; set; } = string.Empty;
-
         public DateTime DateSubmitted { get; set; } = DateTime.Now;
-
         public string SubmittedBy { get; set; } = string.Empty;
-
-        public int NumberOfStudents { get; set; }
-
         public PrincipalApprovalStatus Status { get; set; } = PrincipalApprovalStatus.Pending;
     }
 }
